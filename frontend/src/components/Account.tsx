99e3c01221e8c986433f78488b296d29e394bf7f
oguzhancttnky <oguzhancttnky@gmail.com>
2024-08-11 16:43:24 +0300

    Fixed bugs on account page

--- frontend/src/components/Account.tsx
+++ frontend/src/components/Account.tsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../app/store';
 import axios from 'axios';
 import Navbar from './Navbar';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import TelegramIcon from '../assets/icons/telegram-icon.svg'
 import EmailIcon from '../assets/icons/email-icon.svg'
 import PasswordIcon from '../assets/icons/password-icon.svg'
@@ -10,9 +12,6 @@ import { toast } from "react-toastify";
 import Spinner from './Spinner';
 
 const Account: React.FC = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [chatID, setChatID] = useState('');
     const userID = useSelector((state: RootState) => state.auth.userID);
     const [userEmail, setUserEmail] = useState('');
     const [userChatID, setUserChatID] = useState(0);
@@ -36,45 +35,42 @@ const Account: React.FC = () => {
         getUserData();
     }, [userID]);
 
-    const handleUpdate = async (event: React.FormEvent) => {
-        event.preventDefault();
-        toast.dismiss();
-        setLoading(true);
-        try {
-            if (email === '' && password === '' && chatID === '') {
-                toast.error("Please enter field you want to change");
+    const formik = useFormik({
+        initialValues: {
+            chatID: '',
+            email: '',
+            password: '',
+        },
+        validationSchema: Yup.object({
+            chatID: Yup.number()
+                .typeError('Chat ID must be a number')
+                .positive('Invalid Chat ID')
+                .integer('Invalid Chat ID'),
+            email: Yup.string()
+                .email('Invalid email address'),
+            password: Yup.string()
+                .min(6, 'Password must be at least 6 characters')
+        }),
+        onSubmit: async (values) => {
+            toast.dismiss();
+            setLoading(true);
+            try {
+                const jwtToken = localStorage.getItem('jwtToken');
+                await axios.put(`${process.env.REACT_APP_API_URL}/api/update/user/${userID}`, { email: values.email, password: values.password, chat_id: values.chatID }, {
+                    headers: {
+                        Authorization: `Bearer ${jwtToken}`
+                    }
+                });
                 setLoading(false);
-                return;
-            }
-            if (email === userEmail && chatID === userChatID.toString()) {
-                toast.error("Please enter new email and chat ID");
+                toast.success("Account updated successfully");
+            } catch (err: any) {
+                console.error('Update failed:', err);
                 setLoading(false);
-                return;
+                toast.error("Update failed " + err.response.data.error);
+
             }
-            if (email === userEmail) {
-                toast.error("Please enter a new email");
-                setLoading(false);
-                return;
-            }
-            if (chatID === userChatID.toString()) {
-                toast.error("Please enter a new chat ID");
-                setLoading(false);
-                return;
-            }
-            const jwtToken = localStorage.getItem('jwtToken');
-            await axios.put(`${process.env.REACT_APP_API_URL}/api/update/user/${userID}`, { email, password, chat_id: chatID }, {
-                headers: {
-                    Authorization: `Bearer ${jwtToken}`
-                }
-            });
-            setLoading(false);
-            toast.success("Account updated successfully");
-        } catch (err: any) {
-            console.error('Update failed:', err);
-            setLoading(false);
-            toast.error("Update failed " + err.response.data.error);
-        }
-    }
+        },
+    });
 
     return (
         <div className="mx-auto mt-5">
@@ -83,23 +79,26 @@ const Account: React.FC = () => {
                 <div className='flex items-center justify-center my-8'>
                     <span className="text-gray-900 text-3xl font-semibold">Account Settings</span>
                 </div>
-                <div className="container flex items-center justify-center my-16 px-6 mx-auto">
-                    <form onSubmit={handleUpdate} className="w-full max-w-md">
-                        <div className="relative">
+                <div className="container flex items-center justify-center my-8 px-6 mx-auto">
+                    <form onSubmit={formik.handleSubmit} className="w-full max-w-md">
+                        <div className="relative mt-6">
                             <div className="flex items-center">
                                 <span className="absolute left-0 flex items-center pl-3">
                                     <img src={TelegramIcon.toString()} alt="Telegram Icon" className="w-6 h-6" />
                                 </span>
                                 <input
+                                    {...formik.getFieldProps('chatID')}
                                     type="text"
-                                    className="
+                                    inputMode='numeric'
+                                    className={`${formik.touched.chatID && formik.errors.chatID ? 'border-red-500' : ''}
             block w-full py-3 pl-12 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 
-            focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40`}
                                     placeholder={userChatID === 0 ? 'Please enter Telegram Chat ID' : userChatID.toString()}
-                                    value={chatID}
-                                    onChange={(e) => setChatID(e.target.value)}
                                 />
                             </div>
+                            {formik.touched.chatID && formik.errors.chatID ? (
+                                <div className="text-red-500 text-sm mt-1">{formik.errors.chatID}</div>
+                            ) : null}
                         </div>
                         <div className="relative mt-6">
                             <div className="flex items-center">
@@ -107,15 +106,17 @@ const Account: React.FC = () => {
                                     <img src={EmailIcon.toString()} alt="Email Icon" className="w-6 h-6" />
                                 </span>
                                 <input
+                                    {...formik.getFieldProps('email')}
                                     type="email"
-                                    className="
+                                    className={`${formik.touched.email && formik.errors.email ? 'border-red-500' : ''}
             block w-full py-3 pl-12 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 
-            focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40`}
                                     placeholder={userEmail}
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
                                 />
                             </div>
+                            {formik.touched.email && formik.errors.email ? (
+                                <div className="text-red-500 text-sm mt-1">{formik.errors.email}</div>
+                            ) : null}
                         </div>
 
                         <div className="relative mt-6">
@@ -124,15 +125,17 @@ const Account: React.FC = () => {
                                     <img src={PasswordIcon.toString()} alt="Password Icon" className="w-6 h-6" />
                                 </span>
                                 <input
+                                    {...formik.getFieldProps('password')}
                                     type="password"
-                                    className="
+                                    className={`${formik.touched.password && formik.errors.password ? 'border-red-500' : ''}
             block w-full py-3 pl-12 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 
-            focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40`}
                                     placeholder="Password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
                                 />
                             </div>
+                            {formik.touched.password && formik.errors.password ? (
+                                <div className="text-red-500 text-sm mt-1">{formik.errors.password}</div>
+                            ) : null}
                         </div>
 
 
